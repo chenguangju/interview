9070aee1fe866204e74055aea3c45640e27ddab6
chenguangju <296645438@qq.com>
2022-07-07 10:57:25 +0800

    [add]添加FIFOCache 代码

--- src/main/java/redis/cache/缓存算法/FIFOCache.java
+++ src/main/java/redis/cache/缓存算法/FIFOCache.java
@@ -1,9 +1,26 @@
 package redis.cache.缓存算法;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /**
- * @description:
- * @author: 陈广驹
- * @time: 2022/7/6 14:45
+ * 和LRUCache 实现基本一致，只是把accessOrder 设置为false
  */
-public class FIFOCache {
+public class FIFOCache extends LinkedHashMap<Integer,Integer> {
+
+    private int initialCapacity;
+
+    public FIFOCache(int initialCapacity){
+        super(initialCapacity);
+        this.initialCapacity=initialCapacity;
+    }
+
+    public int get(int key){
+        return super.getOrDefault(key,-1);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size()>initialCapacity;
+    }
 }
